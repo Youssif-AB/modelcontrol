cf425eb3f1310aec2d3bf7015710bcec6377894d
Youssif Abdelaziz <youssif200579@gmail.com>
2026-08-02 23:25:24 -0600

    Configure PostgreSQL database connection

---
+++ backend/app/database.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise RuntimeError("DATABASE_URL is not configured")
+
+engine = create_engine(DATABASE_URL)
