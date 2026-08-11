51836327ee10f172d8d3a8b75df83f70cd47c970
Youssif Abdelaziz <youssif200579@gmail.com>
2026-08-10 21:40:39 -0600

    Add model retrieval endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, UploadFile, Depends, status
+from fastapi import FastAPI, UploadFile, Depends, status, HTTPException
 from app.schemas import ModelCreate
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 from app.database import get_db
 from app.models import ModelRecord
@@ -40,3 +41,24 @@ def create_model(
     db.refresh(record)
 
     return record
+
+@app.get("/models", response_model=list[ModelRead])
+def list_models(db : Session = Depends(get_db)):
+    statement = select(ModelRecord).order_by(ModelRecord.id)
+    models = db.scalars(statement).all()
+    return models
+
+@app.get("/models/{model_id}", response_model=ModelRead)
+def get_model(
+    model_id:int,
+    db: Session = Depends(get_db),
+) -> ModelRecord:
+    model = db.get(ModelRecord, model_id)
+
+    if model is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail = "Model not found"
+        )
+
+    return model
